21541d6614254a1294bec200873cc88b91cbf93c
Kyle Chadha <kyle.chadha@gmail.com>
2014-02-07 11:58:12 -0800

    Create models and controllers

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post
   include Mongoid::Document
+  
   field :title, type: String
   field :desc, type: String
   field :url, type: String
+
+  belongs_to :user
+  has_and_belongs_to_many :categories
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User
   include Mongoid::Document
+  
   field :username, type: String
   field :password_salt, type: String
   field :password_hash, type: String
   field :rep, type: Integer
+
+  has_many :posts
 end
